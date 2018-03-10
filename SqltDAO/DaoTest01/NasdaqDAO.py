d8b3c92e323c3ac064c27ad323a9f0853a0c140c
Soft9000.com <r.a.nagy@gmail.com>
2018-03-10 07:35:39 -0500

    Final Documentation
    Ready for our Meetup!

--- SqltDAO/DaoTest01/NasdaqDAO.py
+++ SqltDAO/DaoTest01/NasdaqDAO.py
@@ -3,7 +3,6 @@ import sqlite3
 
 class NasdaqDAO:
 
-
     def __init__(self):
         self.db = './StockInfo.sqlt3'
         self.conn = None
@@ -12,7 +11,6 @@ class NasdaqDAO:
         self.fields = [('Symbol', 'TEXT'), ('SecurityName', 'TEXT'), ('MarketCategory', 'TEXT'), ('TestIssue', 'TEXT'), ('FinancialStatus', 'TEXT'), ('RoundLotSize', 'INTEGER'), ('ETF', 'TEXT'), ('NextShares', 'TEXT')]
         self.table_name = 'TOK_NASDAQ'
         
-
     def open(self):
         if self.bOpen is False:
             self.conn = sqlite3.connect(self.db)
@@ -20,49 +18,42 @@ class NasdaqDAO:
             self.bOpen = True
         return True
         
-
     def close(self):
         if self.bOpen:
             self.conn.commit()
             self.bOpen = False
         return True
         
-
     def count(self):
         if self.bOpen:
             res = self.curs.execute("SELECT count(*) FROM TOK_NASDAQ;")
             return res.fetchone()[0]
         return -1
         
-
     def drop_table(self):
         if self.bOpen:
             self.curs.execute("DrOp TaBLe IF EXISTS TOK_NASDAQ;")
             return True
         return False
         
-
     def create_table(self):
         if self.bOpen:
             self.curs.execute("CREATE TABLE IF NOT EXISTS TOK_NASDAQ(ID INTEGER PRIMARY KEY AUTOINCREMENT, Symbol TEXT, SecurityName TEXT, MarketCategory TEXT, TestIssue TEXT, FinancialStatus TEXT, RoundLotSize INTEGER, ETF TEXT, NextShares TEXT);")
             return True
         return False
         
-
     def insert(self, fields):
         if self.bOpen:
             self.curs.execute("INSERT INTO TOK_NASDAQ ( Symbol, SecurityName, MarketCategory, TestIssue, FinancialStatus, RoundLotSize, ETF, NextShares) VALUES (?,?,?,?,?,?,?,?);", fields)
             return True
         return False
         
-
     def delete(self, primary_key):
         if self.bOpen:
             self.curs.execute("DELETE from TOK_NASDAQ WHERE ID = ?;", [primary_key])
             return True
         return False
         
-
     def select(self, sql_select):
         if self.bOpen:
             self.curs.execute(sql_select)
@@ -71,4 +62,21 @@ class NasdaqDAO:
                 yield ref
         return None
         
-
+    @staticmethod
+    def Import(dao, data_file='../DaoTest01/nasdaqlisted.txt', hasHeader=True, sep='|'):
+        try:
+            # dao.open()
+            with open(data_file) as fh:
+                line = fh.readline().strip()
+                if hasHeader is True:
+                    line = fh.readline()
+                while len(line) is not 0:
+                    dao.insert(line.split(sep))
+                    line = fh.readline().strip()
+            # dao.close()
+            return True
+        except:
+            pass
+        return False
+        
+    
