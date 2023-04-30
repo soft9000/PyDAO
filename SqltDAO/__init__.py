60f184c1e4efe1946520bb9db511cd65fdd3f895
Randall Nagy <randall.nagy@gmail.com>
2023-04-30 11:51:35 -0400

    Official package support.
    Just copy & include SqltDAO like any other package.

---
+++ SqltDAO/__init__.py
@@ -0,0 +1,9 @@
+'''
+Supporting namespace for generated Soft9000.com PyDAO projects.
+Include this project on your Python path and / or or next to 
+your generated code.
+
+Enjoy!
+
+   -- Randall
+'''
