cdd2ca7aa5eba19a35d5356751074cc1a8f7000e
Randall Nagy <randall.nagy@soft9000.com>
2024-05-27 16:47:38 -0400

    Tighter. 1.3.0

--- SqltDAO/CodeGen01/Meta.py
+++ SqltDAO/CodeGen01/Meta.py
@@ -12,7 +12,7 @@ from SqltDAO.CodeGen01.DaoExceptions import GenOrderError
 
 class Meta:
     PRODUCT = "PyDAO 9000"
-    VERSION = '1.2.8'
+    VERSION = '1.3.0'
     
     @staticmethod
     def Title():
